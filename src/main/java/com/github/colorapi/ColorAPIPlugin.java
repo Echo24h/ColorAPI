3afbbf4847636899922d6eee32db0db84c786617
Echo24h <gregborne.pro@gmail.com>
2023-08-05 14:37:43 +0200

    1.0

--- src/main/java/com/github/colorapi/ColorAPIPlugin.java
+++ src/main/java/com/github/colorapi/ColorAPIPlugin.java
@@ -1,6 +1,8 @@
 package com.github.colorapi;
 
 import com.github.colorapi.commands.Commands;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
